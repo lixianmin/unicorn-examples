4d77193b1b972b08674ece6c2c7069c681e72447
lixianmin <lixianmin@live.cn>
2022-11-10 18:47:18 +0800

    补本地化相关的代码逻辑

--- Assets/Code/Client/GameMetadataManager.cs
+++ Assets/Code/Client/GameMetadataManager.cs
@@ -49,7 +49,7 @@ namespace Client
             {
                 LoadFileAsync("Assets/res/metadata/metadata.bytes", stream => LoadRawStream(stream)),
                 LoadFileAsync("Assets/res/metadata/metadata@.bytes", LoadIncreamentStream),
-                LoadFileAsync("Assets/res/metadata/locale.zh_cn.bytes", LoadLocaleTextStream)
+                LoadFileAsync("Assets/res/metadata/locale.en.bytes", LoadLocaleTextStream)
             };
 
             for (int i = 0; i < nodes.Length;)
